ca1766488cef80d5adc83e9a98c3022022449a7c
Emilio Morles <emiliomorles>
2023-10-16 13:25:40 -0600

    v.1.2

--- main.py
+++ main.py
@@ -33,11 +33,13 @@ while len(guessed_states) < 51:
     # print(answer_state_t)
 
     if answer_state_t == "Exit":
-        missing_states = []
-        for state in us_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        # print(missing_states)
+        missing_states = [state for state in us_states if state not in guessed_states]
+        # I created the same that is in the following 4 lines with a conditional list comprehension.
+        # missing_states = []
+        # for state in us_states:
+        #     if state not in guessed_states:
+        #         missing_states.append(state)
+        # # print(missing_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")  # It creates a file with all the missing states
         break
